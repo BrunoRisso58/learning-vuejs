6d5f00d8b66db4fb9e86fdcff46a4d020c74ff1b
Bruno Risso <brunorisso58@gmail.com>
2022-09-08 12:14:26 -0300

    Diretivas Vue

--- 2/src/components/Info.vue
+++ 2/src/components/Info.vue
@@ -1,12 +1,15 @@
 <template>
     <div>
-        <p>Tenho {{age}} anos.</p>
-        <p>Utilizo as seguintes tecnologias:</p>
-        <ul>
-            <li>VueJS</li>
-            <li>JavaScript</li>
-            <li>CSS</li>
-        </ul>
+        <p v-if="isWorking">Estou trabalhando no momento!</p>
+        <p v-else>Estou em busca de novas oportunidades!</p>
+        <div v-show="workWithTechnology">
+            <p>Utilizo as seguintes tecnologias:</p>
+            <ul>
+                <li>VueJS</li>
+                <li>JavaScript</li>
+                <li>CSS</li>
+            </ul>
+        </div>
     </div>
 </template>
 
@@ -15,7 +18,9 @@
         name: "Info",
         data() {
             return { 
-                age: 19
+                age: 19,
+                isWorking: true,
+                workWithTechnology: true
             }
         }
     }
