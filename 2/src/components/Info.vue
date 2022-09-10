d3ac43c543c8f9a97af279019da2ebb25d77ca72
Bruno Risso <brunorisso58@gmail.com>
2022-09-09 23:57:30 -0300

    Atributos Dinâmicos

--- 2/src/components/Info.vue
+++ 2/src/components/Info.vue
@@ -10,17 +10,38 @@
                 <li>CSS</li>
             </ul>
         </div>
+        <button @click="showOrHideEmail">{{ showEmailButtonMsg }}</button>
+        <p v-show="showEmail">Entre um contato! Mande um e-mail para {{ email }}</p>
     </div>
+    <Picture />
 </template>
 
 <script>
+    import Picture from "./Picture.vue";
+
     export default {
         name: "Info",
         data() {
-            return { 
+            return {
                 age: 19,
                 isWorking: true,
-                workWithTechnology: true
+                workWithTechnology: true,
+                showEmail: false,
+                showEmailButtonMsg: "Mostrar Email",
+                email: "brunorisso58@gmail.com"
+            }
+        },
+        components: {
+            Picture
+        },
+        methods: {
+            showOrHideEmail() {
+                this.showEmail = !this.showEmail;
+                if (this.showEmail) {
+                    this.showEmailButtonMsg = "Ocultar Email";
+                } else {
+                    this.showEmailButtonMsg = "Mostrar Email";
+                }
             }
         }
     }
