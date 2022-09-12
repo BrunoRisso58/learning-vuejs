66823e5986b6c93d6a3ebca92fde9ba6a5dd5d81
Bruno Risso <brunorisso58@gmail.com>
2022-09-12 18:30:16 -0300

    CSS Global

--- 2/src/App.vue
+++ 2/src/App.vue
@@ -1,4 +1,5 @@
 <template>
+    <Cabecalho />
     <PrimeiroComponente />
     <LifeCycle />
     <Pessoa />
@@ -8,6 +9,7 @@
 import PrimeiroComponente from './components/PrimeiroComponente.vue';
 import LifeCycle from './components/LifeCycle.vue';
 import Pessoa from './components/Pessoa.vue';
+import Cabecalho from './components/Cabecalho.vue';
 
     export default {
         name: 'App',
@@ -15,6 +17,21 @@ import Pessoa from './components/Pessoa.vue';
             PrimeiroComponente,
             LifeCycle,
             Pessoa,
+            Cabecalho
         }
     }
 </script>
+
+<style>
+
+    body {
+        background-color: #212F3D;
+        color: #F7F9F9
+    }
+
+    a {
+        color: red;
+        text-decoration: none
+    }
+
+</style>
