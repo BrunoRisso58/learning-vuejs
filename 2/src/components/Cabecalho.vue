fba8a8b9a71c21ef3f621645da2dadeda31b6f3f
Bruno Risso <brunorisso58@gmail.com>
2022-09-12 18:29:48 -0300

    CSS Global e CSS Scoped

---
+++ 2/src/components/Cabecalho.vue
@@ -0,0 +1,28 @@
+<template>
+    <ul>
+        <li><a href="#">Home</a></li>
+        <li><a href="#">Sobre</a></li>
+        <li><a href="#">Contato</a></li>
+    </ul>
+</template>
+
+<script>
+    export default {
+        name: "Header",
+    }
+</script>
+
+<style scoped>
+
+    ul {
+        background-color: #F7F9F9;
+        color: #212F3D;
+        display: flex;
+        list-style-type: none;
+    }
+
+    li {
+        padding: 10px
+    }
+
+</style>
