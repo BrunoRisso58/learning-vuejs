31546605bfa96223228899dab089bd3cb3074284
Bruno Risso <brunorisso58@gmail.com>
2022-09-13 11:22:14 -0300

    Reutilização de Componentes

--- 2/src/components/PrimeiroComponente.vue
+++ 2/src/components/PrimeiroComponente.vue
@@ -1,13 +1,20 @@
 <template>
+    <Reutilizacao />
     <div>
         <h1>Olá Vue!</h1>
         <p>Meu nome é {{nome}} e minha profissão é {{profissao}}</p>
     </div>
+    <Reutilizacao />
     <MultiplosEventos />
+    <Reutilizacao />
+    <Reutilizacao />
+    <Reutilizacao />
+    <Reutilizacao />
 </template>
 
 <script>
 import MultiplosEventos from './MultiplosEventos.vue';
+import Reutilizacao from './Reutilizacao.vue';
 
     export default {
         name: "PrimeiroComponente",
@@ -18,7 +25,8 @@ import MultiplosEventos from './MultiplosEventos.vue';
             }
         },
         components: {
-            MultiplosEventos
+            MultiplosEventos,
+            Reutilizacao
         }
     }
 </script>
---
+++ 2/src/components/Reutilizacao.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+        <button @click="ativar">Clique aqui!</button>
+        <span v-if="ativou">Ativado!</span>
+        <span v-else>Desativado!</span>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "Reutilizacao",
+    data() {
+        return {
+            ativou: false
+        }
+    },
+    methods: {
+        ativar() {
+            this.ativou = !this.ativou;
+        }
+    }
+}
+</script>
