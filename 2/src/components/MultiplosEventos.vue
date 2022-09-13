9aa4817ce2a8698e943ed3361082c50b4f77ef3f
Bruno Risso <brunorisso58@gmail.com>
2022-09-12 23:38:09 -0300

    Múltiplos eventos

---
+++ 2/src/components/MultiplosEventos.vue
@@ -0,0 +1,41 @@
+<template>
+    <div>
+        <button @click="primeiroEvento($event, txt1), segundoEvento($event, txt2), terceiroEvento($event, txt3)">Múltiplos Eventos</button>
+        <p>{{ eventMessage }}</p>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "MultiplosEventos",
+    data() {
+        return {
+            eventMessage: "",
+            txt1: "Primeiro evento",
+            txt2: "Segundo evento",
+            txt3: "Terceiro evento"
+        }
+    },
+    methods: {
+        primeiroEvento(e, txt1) {
+            this.eventMessage = txt1;
+        },
+        segundoEvento(e, txt2) {
+            setTimeout(() => {
+                this.eventMessage = txt2
+            }, 700)
+        },
+        terceiroEvento(e, txt3) {
+            setTimeout(() => {
+                this.eventMessage = txt3
+            }, 1400)
+        }
+    }
+}
+</script>
+
+<style scoped>
+    div {
+        margin: 100px
+    }
+</style>
--- 2/src/components/PrimeiroComponente.vue
+++ 2/src/components/PrimeiroComponente.vue
@@ -3,9 +3,12 @@
         <h1>Olá Vue!</h1>
         <p>Meu nome é {{nome}} e minha profissão é {{profissao}}</p>
     </div>
+    <MultiplosEventos />
 </template>
 
 <script>
+import MultiplosEventos from './MultiplosEventos.vue';
+
     export default {
         name: "PrimeiroComponente",
         data() {
@@ -13,6 +16,9 @@
                 nome: "Bruno",
                 profissao: "QA"
             }
+        },
+        components: {
+            MultiplosEventos
         }
     }
 </script>
